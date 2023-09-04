d1021a34919da4ac87aad3137940eb487329c1b2
vaanessamota <vanessaoliveira2706@gmail.com>
2023-09-03 23:15:04 -0300

    feat(domain) add extension to Domainn Error

--- lib/login/domain/errors/domain_errors.dart
+++ lib/login/domain/errors/domain_errors.dart
@@ -1,1 +1,14 @@
 enum DomainError { unexpected, invalidCredentials, accessDenied }
+
+extension DomainErrorExtension on DomainError {
+  String get description {
+    switch (this) {
+      case DomainError.unexpected:
+        return "Algo errado aconteceu. Tente novamente em breve.";
+      case DomainError.invalidCredentials:
+        return "Credenciais inválidas.";
+      case DomainError.accessDenied:
+        return "Acesso negado. Você não tem permissão para realizar esta ação.";
+    }
+  }
+}
